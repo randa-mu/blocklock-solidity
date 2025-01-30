b24f10f765a28771fe8c02e0b81dab48956f79d7
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-01-30 11:54:58 +0000

    remove unused imports

--- script/BlocklockUpgrade.s.sol
+++ script/BlocklockUpgrade.s.sol
@@ -4,10 +4,6 @@ pragma solidity 0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 
-import {MockBlocklockReceiver} from "../src/mocks/MockBlocklockReceiver.sol";
-import {SignatureSchemeAddressProvider} from "../src/signature-schemes/SignatureSchemeAddressProvider.sol";
-import {BlocklockSignatureScheme} from "../src/blocklock/BlocklockSignatureScheme.sol";
-import {SignatureSender} from "../src/signature-requests/SignatureSender.sol";
 import {BlocklockSender} from "../src/blocklock/BlocklockSender.sol";
 import {DecryptionSender} from "../src/decryption-requests/DecryptionSender.sol";
 import {BLS} from "../src/libraries/BLS.sol";
@@ -15,9 +11,6 @@ import {TypesLib} from "../src/libraries/TypesLib.sol";
 import {UUPSProxy} from "../src/proxy/UUPSProxy.sol";
 
 contract BlocklockUpgradeScript is Script {
-    BlocklockSignatureScheme blocklockSignatureScheme;
-    SignatureSchemeAddressProvider signatureSchemeAddressProvider;
-
     UUPSProxy decryptionSenderProxy;
     DecryptionSender decryptionSenderImplementation;
     DecryptionSender decryptionSenderInstance;
@@ -25,21 +18,6 @@ contract BlocklockUpgradeScript is Script {
     UUPSProxy blocklockSenderProxy;
     BlocklockSender blocklockSenderImplementation;
     BlocklockSender blocklockSenderInstance;
-
-    MockBlocklockReceiver mockBlocklockReceiver;
-
-    string SCHEME_ID = "BN254-BLS-BLOCKLOCK";
-
-    BLS.PointG2 pk = BLS.PointG2({
-        x: [
-            17445541620214498517833872661220947475697073327136585274784354247720096233162,
-            18268991875563357240413244408004758684187086817233527689475815128036446189503
-        ],
-        y: [
-            11401601170172090472795479479864222172123705188644469125048759621824127399516,
-            8044854403167346152897273335539146380878155193886184396711544300199836788154
-        ]
-    });
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
