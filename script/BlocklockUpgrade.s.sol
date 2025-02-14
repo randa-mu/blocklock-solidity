45d06e01f3c6215be91c08c40e1f531c83b08ac4
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-02-14 23:10:22 +0000

    update deployment scripts

--- script/BlocklockUpgrade.s.sol
+++ script/BlocklockUpgrade.s.sol
@@ -11,47 +11,38 @@ import {TypesLib} from "../src/libraries/TypesLib.sol";
 import {UUPSProxy} from "../src/proxy/UUPSProxy.sol";
 
 contract BlocklockUpgradeScript is Script {
-    UUPSProxy decryptionSenderProxy;
-    DecryptionSender decryptionSenderImplementation;
-    DecryptionSender decryptionSenderInstance;
-
-    UUPSProxy blocklockSenderProxy;
-    BlocklockSender blocklockSenderImplementation;
-    BlocklockSender blocklockSenderInstance;
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
 
-        address admin = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
         address decryptionSenderProxyAddr = 0x9297Bb1d423ef7386C8b2e6B7BdE377977FBedd3;
         address blocklockSenderProxyAddr = 0xfF66908E1d7d23ff62791505b2eC120128918F44;
 
-        decryptionSenderImplementation = new DecryptionSender();
+        DecryptionSender decryptionSenderImplementation = new DecryptionSender();
         console.log("\nDecryptionSender implementation contract deployed at: ", address(decryptionSenderImplementation));
 
-        decryptionSenderProxy = UUPSProxy(payable(decryptionSenderProxyAddr));
+        UUPSProxy decryptionSenderProxy = UUPSProxy(payable(decryptionSenderProxyAddr));
         console.log("DecryptionSender proxy contract deployed at: ", address(decryptionSenderProxy));
 
-        blocklockSenderImplementation = new BlocklockSender();
+        BlocklockSender blocklockSenderImplementation = new BlocklockSender();
         console.log("\nBlocklockSender implementation contract deployed at: ", address(blocklockSenderImplementation));
 
-        blocklockSenderProxy = UUPSProxy(payable(blocklockSenderProxyAddr));
+        UUPSProxy blocklockSenderProxy = UUPSProxy(payable(blocklockSenderProxyAddr));
         console.log("BlocklockSender proxy contract deployed at: ", address(blocklockSenderProxy));
 
-        decryptionSenderInstance = DecryptionSender(address(decryptionSenderProxy));
-        blocklockSenderInstance = BlocklockSender(address(blocklockSenderProxy));
+        DecryptionSender decryptionSender = DecryptionSender(address(decryptionSenderProxy));
+        BlocklockSender blocklockSender = BlocklockSender(address(blocklockSenderProxy));
 
-        console.log("\nDecryptionSender version pre upgrade: ", decryptionSenderInstance.version());
-        console.log("BlocklockSender version pre upgrade: ", blocklockSenderInstance.version());
+        console.log("\nDecryptionSender version pre upgrade: ", decryptionSender.version());
+        console.log("BlocklockSender version pre upgrade: ", blocklockSender.version());
 
         // Perform implementation contract upgrades
-        decryptionSenderInstance.upgradeToAndCall(address(decryptionSenderImplementation), "");
-        blocklockSenderInstance.upgradeToAndCall(address(blocklockSenderImplementation), "");
+        decryptionSender.upgradeToAndCall(address(decryptionSenderImplementation), "");
+        blocklockSender.upgradeToAndCall(address(blocklockSenderImplementation), "");
 
-        console.log("\nDecryptionSender version post upgrade: ", decryptionSenderInstance.version());
-        console.log("BlocklockSender version post upgrade: ", blocklockSenderInstance.version());
+        console.log("\nDecryptionSender version post upgrade: ", decryptionSender.version());
+        console.log("BlocklockSender version post upgrade: ", blocklockSender.version());
 
         vm.stopBroadcast();
     }
