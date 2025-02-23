ce8f1c7fcc67f5ceaf64737efcc6cc05665156e9
L0GYKAL.eth <32228897+L0GYKAL@users.noreply.github.com>
2025-02-23 13:30:30 +0100

    Update AbstractBlocklockReceiver.sol

--- src/AbstractBlocklockReceiver.sol
+++ src/AbstractBlocklockReceiver.sol
@@ -5,15 +5,19 @@ import {IBlocklockReceiver} from "./interfaces/IBlocklockReceiver.sol";
 import {IBlocklockSender} from "./interfaces/IBlocklockSender.sol";
 
 abstract contract AbstractBlocklockReceiver is IBlocklockReceiver {
-    IBlocklockSender public blocklock;
+    IBlocklockSender public blocklockProvider;
 
     modifier onlyBlocklockContract() {
-        require(msg.sender == address(blocklock), "Only timelock contract can call this.");
+        require(msg.sender == address(blocklockProvider), "Only timelock contract can call this.");
         _;
     }
 
     constructor(address blocklockSender) {
-        blocklock = IBlocklockSender(blocklockSender);
+        blocklockProvider = IBlocklockSender(blocklockSender);
+    }
+
+    function requestBlocklock(uint256 blockHeight, TypesLib.Ciphertext calldata ciphertext) internal returns (uint256 requestID) {
+        requestID = blocklockProvider.requestBlocklock(uint256 blockHeight, TypesLib.Ciphertext calldata ciphertext);
     }
 
     function receiveBlocklock(uint256 requestID, bytes calldata decryptionKey) external virtual onlyBlocklockContract {
