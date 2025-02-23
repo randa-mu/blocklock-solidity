7f5cdc0c5b161a78f9753530c6fe3d31b0a42fca
L0GYKAL.eth <32228897+L0GYKAL@users.noreply.github.com>
2025-02-23 13:21:17 +0100

    feat(AbstractBlocklockReceiver): add onBlocklockReceived

--- src/AbstractBlocklockReceiver.sol
+++ src/AbstractBlocklockReceiver.sol
@@ -17,4 +17,13 @@ abstract contract AbstractBlocklockReceiver is IBlocklockReceiver {
     }
 
     function receiveBlocklock(uint256 requestID, bytes calldata decryptionKey) external virtual onlyBlocklockContract {}
+
+    /**
+     * @notice Handles the reception of a generated random value for a specific request.
+     * @dev This internal function is called when randomness is received for the given `requestID`.
+     * It is intended to be overridden by derived contracts to implement custom behavior.
+     * @param requestID The unique identifier of the randomness request.
+     * @param decryptionKey The key required for the decryption associated with this requestID, provided as a `bytes32` type.
+     */
+    function onBlocklockReceived(uint256 requestID, bytes calldata decryptionKey) internal virtual;
 }
