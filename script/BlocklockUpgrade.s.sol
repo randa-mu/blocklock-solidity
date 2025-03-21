069bc7dfe83e932ca1e5d52ba756202185be6a6d
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-03-21 11:24:34 +0000

    update code documentation

--- script/BlocklockUpgrade.s.sol
+++ script/BlocklockUpgrade.s.sol
@@ -47,17 +47,3 @@ contract BlocklockUpgradeScript is Script {
         vm.stopBroadcast();
     }
 }
-
-/**
- * # Deployment steps
- *
- * ## STEP 1. Load the variables in the .env file
- * source .env
- *
- * ## STEP 2. Deploy and verify the contract
- * forge script script/BlocklockUpgrade.s.sol:BlocklockUpgradeScript --rpc-url $CALIBRATIONNET_RPC_URL --broadcast -g 100000 -vvvv
- *
- * -g is the gas limit passed in order to prevent a common error with deploying contracts to the FEVM as per the docs in the filecoin fevm foundry kit here - https://github.com/filecoin-project/fevm-foundry-kit/tree/main
- *
- * For ethereum, add --verify with etherscan key in .env and foundry.toml files
- */
