bb557a62bea37449ac3a0e73ff85c68dd92336ae
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-04-04 11:18:24 +0100

    add subscription management logic to AbstractBlocklockReceiver

--- src/AbstractBlocklockReceiver.sol
+++ src/AbstractBlocklockReceiver.sol
@@ -51,6 +51,51 @@ abstract contract AbstractBlocklockReceiver is IBlocklockReceiver, ConfirmedOwne
         subscriptionId = subId;
     }
 
+    /// @notice Sets the address of the IBlocklockSender contract.
+    /// @dev Only the contract owner can call this function.
+    /// @param _blocklockSender The address of the deployed sender contract.
+    function setBlocklockSender(address _blocklockSender) external onlyOwner {
+        require(_blocklockSender != address(0), "Cannot set zero address as sender");
+        blocklock = IBlocklockSender(_blocklockSender);
+    }
+
+    /// @notice Adds a list of consumer addresses to the Randamu subscription.
+    /// @dev Requires the subscription ID to be set before calling.
+    /// @param consumers An array of addresses to be added as authorized consumers.
+    function updateSubscription(address[] calldata consumers) external onlyOwner {
+        require(subscriptionId != 0, "subID not set");
+        for (uint256 i = 0; i < consumers.length; i++) {
+            blocklock.addConsumer(subscriptionId, consumers[i]);
+        }
+    }
+
+    /// @notice Creates a new Randamu subscription if none exists and registers this contract as a consumer.
+    /// @dev Internal helper that initializes the subscription only once.
+    /// @return The subscription ID that was created or already exists.
+    function _subscribe() internal returns (uint256) {
+        if (subscriptionId == 0) {
+            subscriptionId = blocklock.createSubscription();
+            blocklock.addConsumer(subscriptionId, address(this));
+        }
+        return subscriptionId;
+    }
+
+    /// @notice Creates and funds a new Randamu subscription using native currency.
+    /// @dev Only callable by the contract owner. If a subscription already exists, it will not be recreated.
+    /// @dev The ETH value sent in the transaction (`msg.value`) will be used to fund the subscription.
+    function createSubscriptionAndFundNative() external payable onlyOwner {
+        _subscribe();
+        blocklock.fundSubscriptionWithNative{value: msg.value}(subscriptionId);
+    }
+
+    /// @notice Tops up the Randamu subscription using native currency (e.g., ETH).
+    /// @dev Requires a valid subscription ID to be set before calling.
+    /// @dev The amount to top up should be sent along with the transaction as `msg.value`.
+    function topUpSubscriptionNative() external payable {
+        require(subscriptionId != 0, "sub not set");
+        blocklock.fundSubscriptionWithNative{value: msg.value}(subscriptionId);
+    }
+
     /// @notice getBalance returns the native balance of the consumer contract
     /// @notice For direct funding requests, the contract needs to hold native tokens to
     /// sufficient enough to cover the cost of the request.
