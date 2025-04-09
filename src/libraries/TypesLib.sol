3547d2616afaa8a98f09462e8b660a8dd162e8d5
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-04-09 17:24:28 +0100

    remove unused code

--- src/libraries/TypesLib.sol
+++ src/libraries/TypesLib.sol
@@ -13,34 +13,11 @@ library TypesLib {
         address callback; // the requester address to call back. Must implement ISignatureReceiver interface to support the required callback
     }
 
-    // fixme remove
-    // // Blocklock request stores details needed to generate blocklock decryption keys
-    // struct BlocklockRequest {
-    //     uint256 decryptionRequestID;
-    //     uint256 blockHeight;
-    //     Ciphertext ciphertext;
-    //     bytes signature;
-    //     bytes decryptionKey;
-    //     address callback;
-    // }
-
     struct Ciphertext {
         BLS.PointG2 u;
         bytes v;
         bytes w;
     }
-
-    // fixme remove
-    // // Decryption request stores details for each decryption request
-    // struct DecryptionRequest {
-    //     string schemeID; // signature scheme id, e.g., "BN254", "BLS12-381", "TESS"
-    //     bytes ciphertext;
-    //     bytes condition;
-    //     bytes decryptionKey;
-    //     bytes signature;
-    //     address callback;
-    //     bool isFulfilled;
-    // }
 
     struct BlocklockRequest {
         uint256 subId; // 0 for direct funding
