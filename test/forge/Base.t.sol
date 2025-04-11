986bad76435b72216b0abf6fc4e3947f82e74cd4
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-04-11 13:38:50 +0100

    update unit tests

--- test/forge/Base.t.sol
+++ test/forge/Base.t.sol
@@ -3,12 +3,24 @@ pragma solidity ^0.8;
 
 import {Test} from "forge-std/Test.sol";
 
+import {TypesLib} from "../../src/libraries/TypesLib.sol";
+import {BLS} from "../../src/libraries/BLS.sol";
+
 /// @title Base test contract
 /// @dev Provides core functionalities needed by all other tests.
 abstract contract Base is Test {
     address internal admin;
     address internal alice;
     address internal bob;
+
+    struct CiphertextDataUint {
+        uint256 plaintext;
+        TypesLib.Ciphertext ciphertext;
+        bytes signature;
+        bytes decryptionKey;
+    }
+
+    mapping(uint256 => CiphertextDataUint) internal ciphertextDataUint;
 
     function setUp() public virtual {
         admin = makeAddr("admin");
@@ -18,6 +30,43 @@ abstract contract Base is Test {
         vm.deal(admin, 10 ether);
         vm.deal(alice, 10 ether);
         vm.deal(bob, 10 ether);
+
+
+        generateCiphertextData();
+    }
+
+    function generateCiphertextData() internal {
+        // generate and store ciphertexts and keys
+
+        // 1. 3 ether
+        uint256 plaintext = 3 ether;
+
+        TypesLib.Ciphertext memory ciphertext = TypesLib.Ciphertext({
+            u: BLS.PointG2({
+                x: [
+                    14142380308423906610328325205633754694002301558654408701934220147059967542660,
+                    4795984740938726483924720262587026838890051381570343702421443260575124596446
+                ],
+                y: [
+                    13301122453285478420056122708237526083484415709254283392885579853639158169617,
+                    11125759247493978573666410429063118092803139083876927879642973106997490249635
+                ]
+            }),
+            v: hex"63f745f4240f4708db37b0fa0e40309a37ab1a65f9b1be4ac716a347d4fe57fe",
+            w: hex"e8aadd66a9a67c00f134b1127b7ef85046308c340f2bb7cee431bd7bfe950bd4"
+        });
+
+        bytes memory signature =
+            hex"02b3b2fa2c402d59e22a2f141e32a092603862a06a695cbfb574c440372a72cd0636ba8092f304e7701ae9abe910cb474edf0408d9dd78ea7f6f97b7f2464711";
+
+        bytes memory decryptionKey = hex"7ec49d8f06b34d8d6b2e060ea41652f25b1325fafb041bba9cf24f094fbca259";
+
+        ciphertextDataUint[3 ether] = CiphertextDataUint({
+            plaintext: 3 ether,
+            ciphertext: ciphertext,
+            signature: signature,
+            decryptionKey: decryptionKey
+        });
     }
 
     function signers() internal view returns (address[] memory) {
--- test/forge/Blocklock.t.sol
+++ test/forge/Blocklock.t.sol
@@ -12,35 +12,12 @@ import {
     MockBlocklockReceiver
 } from "./Deployment.t.sol";
 
-import {TypesLib} from "../../src/libraries/TypesLib.sol";
-import {BLS} from "../../src/libraries/BLS.sol";
+import {TypesLib, BLS} from "./Base.t.sol";
 
 /// @title BlocklockTest test contract
 /// @dev Onchain conditional encryption tests
 contract BlocklockTest is Deployment {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
-    uint256 plaintext = 3 ether;
-
-    TypesLib.Ciphertext ciphertext = TypesLib.Ciphertext({
-        u: BLS.PointG2({
-            x: [
-                14142380308423906610328325205633754694002301558654408701934220147059967542660,
-                4795984740938726483924720262587026838890051381570343702421443260575124596446
-            ],
-            y: [
-                13301122453285478420056122708237526083484415709254283392885579853639158169617,
-                11125759247493978573666410429063118092803139083876927879642973106997490249635
-            ]
-        }),
-        v: hex"63f745f4240f4708db37b0fa0e40309a37ab1a65f9b1be4ac716a347d4fe57fe",
-        w: hex"e8aadd66a9a67c00f134b1127b7ef85046308c340f2bb7cee431bd7bfe950bd4"
-    });
-
-    bytes signature =
-        hex"02b3b2fa2c402d59e22a2f141e32a092603862a06a695cbfb574c440372a72cd0636ba8092f304e7701ae9abe910cb474edf0408d9dd78ea7f6f97b7f2464711";
-
-    bytes decryptionKey = hex"7ec49d8f06b34d8d6b2e060ea41652f25b1325fafb041bba9cf24f094fbca259";
 
     SignatureSchemeAddressProvider internal signatureSchemeAddressProvider;
     BlocklockSignatureScheme internal blocklockScheme;
@@ -80,7 +57,9 @@ contract BlocklockTest is Deployment {
         uint32 fulfillmentFlatFeeNativePPM = 1_000_000;
         uint8 nativePremiumPercentage = 10;
 
-        setBlocklockSenderBillingConfiguration(maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage);
+        setBlocklockSenderBillingConfiguration(
+            maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage
+        );
 
         // get request price
         uint32 callbackGasLimit = 100_000;
@@ -104,7 +83,7 @@ contract BlocklockTest is Deployment {
         vm.prank(alice);
         uint32 requestCallbackGasLimit = 100000;
         (uint256 requestId,) =
-            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertext);
+            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertextDataUint[3 ether].ciphertext);
 
         // fetch request information including callbackGasLimit from decryption sender
         TypesLib.DecryptionRequest memory decryptionRequest = decryptionSender.getRequest(requestId);
@@ -148,7 +127,7 @@ contract BlocklockTest is Deployment {
         uint256 gasBefore = gasleft();
 
         vm.prank(admin);
-        decryptionSender.fulfillDecryptionRequest(requestId, decryptionKey, signature);
+        decryptionSender.fulfillDecryptionRequest(requestId, ciphertextDataUint[3 ether].decryptionKey, ciphertextDataUint[3 ether].signature);
 
         uint256 gasAfter = gasleft();
         uint256 gasUsed = gasBefore - gasAfter;
@@ -162,7 +141,7 @@ contract BlocklockTest is Deployment {
 
         decryptionRequest = decryptionSender.getRequest(requestId);
         assertTrue(decryptionRequest.isFulfilled, "Decryption key not provided in decryption sender by offchain oracle");
-        assertTrue(mockBlocklockReceiver.plainTextValue() == plaintext, "Plaintext values mismatch after decryption");
+        assertTrue(mockBlocklockReceiver.plainTextValue() == ciphertextDataUint[3 ether].plaintext, "Plaintext values mismatch after decryption");
         assertTrue(mockBlocklockReceiver.requestId() == 1, "Request id in receiver contract is incorrect");
 
         // check deductions from user and withdrawable amount in blocklock sender for admin
@@ -200,7 +179,9 @@ contract BlocklockTest is Deployment {
         uint32 fulfillmentFlatFeeNativePPM = 1_000_000;
         uint8 nativePremiumPercentage = 10;
 
-        setBlocklockSenderBillingConfiguration(maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage);
+        setBlocklockSenderBillingConfiguration(
+            maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage
+        );
 
         // get request price
         uint32 callbackGasLimit = 100_000;
@@ -224,12 +205,11 @@ contract BlocklockTest is Deployment {
         vm.prank(alice);
         uint32 requestCallbackGasLimit = 100000;
         (uint256 requestId,) =
-            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertext);
-
+            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertextDataUint[3 ether].ciphertext);
 
         vm.startPrank(admin);
         vm.expectRevert("Only blocklock contract can call");
-        mockBlocklockReceiver.receiveBlocklock(requestId, decryptionKey);
+        mockBlocklockReceiver.receiveBlocklock(requestId, ciphertextDataUint[3 ether].decryptionKey);
 
         assert(mockBlocklockReceiver.plainTextValue() == 0);
         assert(mockBlocklockReceiver.requestId() == 1);
@@ -246,7 +226,9 @@ contract BlocklockTest is Deployment {
         uint32 fulfillmentFlatFeeNativePPM = 1_000_000;
         uint8 nativePremiumPercentage = 10;
 
-        setBlocklockSenderBillingConfiguration(maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage);
+        setBlocklockSenderBillingConfiguration(
+            maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage
+        );
 
         // get request price
         uint32 callbackGasLimit = 100_000;
@@ -270,11 +252,11 @@ contract BlocklockTest is Deployment {
         vm.prank(alice);
         uint32 requestCallbackGasLimit = 100000;
         (uint256 requestId,) =
-            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertext);
+            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertextDataUint[3 ether].ciphertext);
 
         vm.expectRevert("No request with specified requestID");
         vm.prank(admin);
-        decryptionSender.fulfillDecryptionRequest(requestId + 1, decryptionKey, signature);
+        decryptionSender.fulfillDecryptionRequest(requestId + 1, ciphertextDataUint[3 ether].decryptionKey, ciphertextDataUint[3 ether].signature);
 
         assert(mockBlocklockReceiver.plainTextValue() == 0);
         assert(mockBlocklockReceiver.requestId() == 1);
@@ -291,7 +273,9 @@ contract BlocklockTest is Deployment {
         uint32 gasAfterPaymentCalculation = 400_000;
         uint32 fulfillmentFlatFeeNativePPM = 1_000_000;
         uint8 nativePremiumPercentage = 10;
-        setBlocklockSenderBillingConfiguration(maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage);
+        setBlocklockSenderBillingConfiguration(
+            maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage
+        );
 
         // get request price
         uint32 callbackGasLimit = 100_000;
@@ -315,13 +299,13 @@ contract BlocklockTest is Deployment {
         vm.prank(alice);
         uint32 requestCallbackGasLimit = 100000;
         (uint256 requestId,) =
-            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertext);
+            mockBlocklockReceiver.createTimelockRequestWithDirectFunding(requestCallbackGasLimit, 13, ciphertextDataUint[3 ether].ciphertext);
 
         vm.startPrank(admin);
         bytes memory invalidSignature =
             hex"02a3b2fa2c402d59e22a2f141e32a092603862a06a695cbfb574c440372a72cd0636ba8092f304e7701ae9abe910cb474edf0408d9dd78ea7f6f97b7f2464711";
         vm.expectRevert("Signature verification failed");
-        decryptionSender.fulfillDecryptionRequest(requestId, decryptionKey, invalidSignature);
+        decryptionSender.fulfillDecryptionRequest(requestId, ciphertextDataUint[3 ether].decryptionKey, invalidSignature);
 
         assert(mockBlocklockReceiver.plainTextValue() == 0);
         assert(mockBlocklockReceiver.requestId() == 1);
@@ -329,7 +313,12 @@ contract BlocklockTest is Deployment {
     }
 
     // helper functions
-    function setBlocklockSenderBillingConfiguration(uint32 maxGasLimit, uint32 gasAfterPaymentCalculation, uint32 fulfillmentFlatFeeNativePPM, uint8 nativePremiumPercentage) internal {
+    function setBlocklockSenderBillingConfiguration(
+        uint32 maxGasLimit,
+        uint32 gasAfterPaymentCalculation,
+        uint32 fulfillmentFlatFeeNativePPM,
+        uint8 nativePremiumPercentage
+    ) internal {
         vm.prank(admin);
         blocklockSender.setConfig(
             maxGasLimit, gasAfterPaymentCalculation, fulfillmentFlatFeeNativePPM, nativePremiumPercentage
